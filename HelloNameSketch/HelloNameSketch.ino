d5b3ba196e10f91ec2c5aaebea29dd334d758df2
Pavel Kaliukhovich <kaliukhovich.pavel@gmail.com>
2024-04-30 19:35:12 +0200

    Output I love dotnet to LCD

--- HelloNameSketch/HelloNameSketch.ino
+++ HelloNameSketch/HelloNameSketch.ino
@@ -1,9 +1,13 @@
 #include <LiquidCrystal.h>;
 
-char *names[] = {"Katya", "Artsiom", "Natallia", "Dymusia"};
-int index = 0;
+const int rs = 12;
+const int en = 11;
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int d4 = 5;
+const int d5 = 4;
+const int d6 = 3;
+const int d7 = 2;
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
@@ -14,18 +18,11 @@ void setup() {
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("I love .NET");
 
-  for (int index = 0; index < 4; index++)
-  {
-    lcd.setCursor(0, 1);
-    lcd.print("Hello, ");
-    lcd.print(names[index]);
-
-    delay(1000);
-    lcd.clear();
-    delay(1000);
-  }
-
-  index = 0;
+  delay(3000);
+  lcd.clear();
+  delay(1000);
 }
 
